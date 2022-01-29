4d71dcff11e39ea6b906146050ce9a33dd98f8c6
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-01-29 22:27:12 +0800

    Refactor: remove sub-module openjpeg.

--- src/lib.rs
+++ src/lib.rs
@@ -29,8 +29,27 @@ pub(crate) use format::*;
 pub mod error;
 pub(crate) use error::*;
 
-mod openjpeg;
-pub use openjpeg::*;
+pub(crate) use openjpeg_sys as sys;
+
+impl From<J2KFormat> for sys::CODEC_FORMAT {
+  fn from(format: J2KFormat) -> Self {
+    match format {
+      J2KFormat::JP2 => sys::CODEC_FORMAT::OPJ_CODEC_JP2,
+      J2KFormat::J2K => sys::CODEC_FORMAT::OPJ_CODEC_J2K,
+    }
+  }
+}
+
+pub(crate) mod codec;
+pub(crate) mod dump;
+pub(crate) mod image;
+pub(crate) mod stream;
+
+pub use codec::*;
+pub use dump::*;
+pub(crate) use stream::*;
+
+pub use self::image::*;
 
 /// Image color space.
 #[derive(Debug, Clone, Copy)]
